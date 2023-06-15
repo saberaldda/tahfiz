5b169073a55d82e25cfbe6de9eb6150d4abfae55
Saber <saberaldda@gmail.com>
2023-06-15 14:10:00 +0300

    change on top points list

--- resources/views/home/top-points.blade.php
+++ resources/views/home/top-points.blade.php
@@ -8,6 +8,7 @@
   <title>لوحة المتفوقين</title>
   <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
   <script src="https://cdn.tailwindcss.com"></script>
+  {{-- @vite('resources/css/app.css') --}}
   <style>
     @font-face {
       font-family: "Barada";
@@ -42,8 +43,6 @@
       animation-iteration-count: infinite;
       animation-timing-function: linear;
     }
-  </style>
-  <style>
     @keyframes scroll {
       0% {
         transform: translateY(0);
@@ -55,6 +54,7 @@
     }
   
     .scrolling-div {
+      animation-delay: 1000ms;
       animation-name: scroll;
       animation-duration: 100s;
       /* Adjust the duration as needed */
@@ -72,14 +72,12 @@
     $shape4Url = asset('assets/shape4.png');
   @endphp
 
-  <div class="min-h-screen bg-emerald-400 relative removeScroll"
-    style="background-image: url({{ $backPatternUrl }})">
-    <div class="container mx-auto px-5 flex flex-col justify-center max-w-[75%] h-full relative z-10">
-      <div class="sticky top-0 left-0 right-0 z-10 bg-emerald-400 w-full"
-        style="background-image: url({{ $backPatternUrl }})">
-        <p class="text-6xl font-bold text-center mt-5 mb-5"> مركز تحفيظ مسجد عمر بن الخطاب </p>
-        <p class="text-3xl font-bold text-center mt-5 mb-3"> لوحة المتفوقين </p>
-      </div>
+  <div class="min-h-screen bg-emerald-400 relative removeScroll" style="background-image: url({{ $backPatternUrl }})">
+    <div class="sticky top-0 left-0 right-0 z-50 bg-emerald-400 w-full" style="background-image: url({{ $backPatternUrl }})">
+      <p class="text-4xl sm:text-5xl md:text-7xl font-bold text-center px-3 pt-5 sm:pt-10 sm:mb-7 mb-2">مركز تحفيظ مسجد عمر بن الخطاب</p>
+      <p class="text-2xl sm:text-3xl font-bold text-center sm:mt-5 pb-3">لوحة المتفوقين</p>
+    </div>
+    <div class="container mx-auto px-7 flex flex-col justify-center sm:max-w-[75%] h-full relative z-10">
       <!-- items -->
         @livewire('home.top-points')
     </div>
--- resources/views/livewire/home/top-points.blade.php
+++ resources/views/livewire/home/top-points.blade.php
@@ -2,12 +2,13 @@
     @foreach ($users as $user)
     <div class="bg-[#BD8A36] rounded-lg p-4 mb-7" style="background-image: url({{ asset('assets/back-pattern.png') }})">
         <div class="border rounded-lg p-3 flex justify-between items-center bg-gray-200" dir="rtl">
-            <div class="flex gap-7 items-center">
-                <p class="font-semibold text-4xl me-2">{{ $loop->iteration }}</p>
-                <img class="w-20 h-20 rounded-full" src="{{ $user->full_photo_path }}" alt="Rounded avatar" />
-                <p class="font-semibold text-4xl me-2">{{ $user->name }}</p>
+            <div class="flex gap-2 sm:gap-4 md:gap-6 items-center">
+                <p class="font-semibold text-2xl sm:text-4xl me-2">{{ $loop->iteration }}</p>
+                <img class="w-16 h-16 sm:w-20 sm:h-20 rounded-full" src="{{ $user->full_photo_path }}"
+                    alt="Rounded avatar" />
+                <p class="font-semibold text-2xl sm:text-4xl me-2">{{ $user->name }}</p>
             </div>
-            <p class="font-medium text-4xl text-gray-600">{{ $user->total_points }} نقطة</p>
+            <p class="font-medium text-2xl sm:text-4xl text-gray-600">{{ $user->total_points }} نقطة</p>
         </div>
     </div>
     @endforeach
