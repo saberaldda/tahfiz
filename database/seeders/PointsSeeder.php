f36c29194c4bb99d3e32a1f147a614abf9f9cca3
Saber <saberaldda@gmail.com>
2023-10-06 23:04:00 +0300

    fix

--- database/seeders/PointsSeeder.php
+++ database/seeders/PointsSeeder.php
@@ -9,7 +9,7 @@ class PointsSeeder extends Seeder
 {
     public function run(): void
     {
-        for ($i = 0; $i < 100000; $i++) {
+        for ($i = 0; $i < 5000; $i++) {
             try {
                 Point::factory(1)->create();
             } catch (\Throwable $th) {
